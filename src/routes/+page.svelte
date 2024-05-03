e573155e9508f2bfcb2a6bae56bc47cf2228d4a9
Raul Melo <melo.raulf@gmail.com>
2024-05-03 17:24:08 +0200

    pin dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+</script>
+
+<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+  Bug report
+</h1>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>Open</DropdownMenu.Trigger>
+  <DropdownMenu.Content>
+    <DropdownMenu.Group>
+      <DropdownMenu.Label>My Account</DropdownMenu.Label>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>Profile</DropdownMenu.Item>
+      <DropdownMenu.Item>Billing</DropdownMenu.Item>
+      <DropdownMenu.Item>Team</DropdownMenu.Item>
+      <DropdownMenu.Item>Subscription</DropdownMenu.Item>
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
